aa9ea8353cfd639e96873b6a1c369c061a6ab80b
Firdaus Riyanto <firdaus.riyanto@fazzfinancial.com>
2021-10-19 18:12:40 +0700

    Add get animation json and localize scripts

--- inc/enqueue-script.php
+++ inc/enqueue-script.php
@@ -4,6 +4,8 @@
 ** link@ https://developer.wordpress.org/themes/basics/including-css-javascript/
 **/
 function add_enqueue_scripts() {
+  $theme_version = wp_get_theme()->get( 'Version' );
+
   // slick 
   //wp_enqueue_style( 'slick', get_template_directory_uri() . '/assets/slick/slick.min.css' );
   //wp_enqueue_script( 'slick', get_template_directory_uri() . '/assets/slick/slick.min.js', array ( 'jquery' ), 1.1, true );
@@ -12,10 +14,33 @@ function add_enqueue_scripts() {
   // wp_enqueue_script( 'gsap', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.3.4/gsap.min.js', array(), 3.3, true );
   // wp_enqueue_script( 'scroll-trigger', 'https://cdnjs.cloudflare.com/ajax/libs/ScrollTrigger/1.0.4/ScrollTrigger.min.js', array(), 1.0, true );
   // general
-  wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/css/main.css', array(),  wp_rand() );
+  $directory = trailingslashit( get_template_directory_uri() );
 
+  //
+  wp_enqueue_style( 'main', $directory . 'assets/css/main.css', array(), $theme_version, 'all' );
 
-  wp_enqueue_script( 'scripts', get_template_directory_uri() . '/assets/js/main.bundle.js', array (), 1.1, true );
-  wp_localize_script( 'scripts', 'ajaxobject', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+  //
+  wp_enqueue_script( 'add-ajaxobject' );
+  wp_enqueue_script( 'scripts', $directory . 'assets/js/main.bundle.js', array(), $theme_version, true  );
+
+  //
+  $url = $directory . 'assets/json/payfazz-suites-15fps-2880.json';
+
+  $request = wp_remote_get( $url, array( 'sslverify' => FALSE ) );
+
+  if ( is_wp_error( $request ) ) {
+      return false; // Bail early
+  }
+
+  //
+  $body = wp_remote_retrieve_body( $request );
+  $json = utf8_encode( $body );
+  $data = json_decode( $json );
+
+  wp_localize_script( 'scripts', 'ajaxobject', array( 
+      'ajaxurl' => admin_url( 'admin-ajax.php' ),
+      'data' => $data
+    )
+  );
 }
 add_action( 'wp_enqueue_scripts', 'add_enqueue_scripts' );
