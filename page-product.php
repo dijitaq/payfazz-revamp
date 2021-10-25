5d06e98173835782df35d9a88238fecd4a592ed3
DESKTOP-8JJ7F6C\Firdaus Riyanto <firdausriyanto@gmail.com>
2021-10-25 17:51:55 +0700

    refactor markup for product page

--- page-product.php
+++ page-product.php
@@ -3,49 +3,97 @@
 
 get_header( 'product' ); ?>
 
-<div class="product-section product-section--hero-image">
-  <div class="grid product-section__grid postition-relative">
+<section class="product-section product-section__hero-image">
+  <div class="grid postition-relative">
     <div class="grid__row postition-relative" style="z-index: 2;">
-      <div class="grid__col product-section__col-5">
-        <h1 class="product-section__heading-one--hero-image">Limitless to<br>Open Business.</h1>
+      <div class="grid__col grid__col--5">
+        <h1 class="product-section__hero-image__heading-one">Limitless to<br>Open Business.</h1>
 
-        <p class="product-section__paragraph--hero-image">PAYFAZZ helps business to access financial services,<br>digital and wholesales goods easily in one ecosystem.</p>
+        <p class="product-section__paragraph product-section__hero-image__paragraph">PAYFAZZ helps business to access financial services,<br>digital and wholesales goods easily in one ecosystem.</p>
       </div>
 
-      <div class="grid__col product-section__col-6 postition-relative">
-        <div class="product-section__thumbnail--hero-image product-section__thumbnail--hero-image-one">
-          <div class="product-section--hero-image__mask">
+      <div class="grid__col grid__col--6 postition-relative">
+        <div class="product-section__hero-image__thumbnail product-section__hero-image__thumbnail--one">
+          <div class="product-section__hero-image__mask">
             <img src="<?php echo get_template_directory_uri() . '/assets/img/product/payfazz-product-hero-image-01.jpg'; ?>">
           </div>
 
-          <span class="product-section--hero-image__flag product-section--hero-image__flag--yellow">Cheap</span>
+          <span class="product-section__hero-image__flag product-section__hero-image__flag--yellow">Cheap</span>
         </div>
 
-        <div class="product-section__phone-container--hero-image">
+        <div class="product-section__hero-image__phone-container">
           <img src="<?php echo get_template_directory_uri() . '/assets/img/product/payfazz-hero-image-phone.png'; ?>">
         </div>
       </div>
     </div>
 
     <div class="grid__row">
-      <div class="grid__col">
-        <div class="product-section__thumbnail--hero-image product-section__offset-2">
-          <div class="product-section--hero-image__mask">
+      <div class="grid__col grid__offset--2">
+        <div class="product-section__hero-image__thumbnail">
+          <div class="product-section__hero-image__mask">
             <img src="<?php echo get_template_directory_uri() . '/assets/img/product/payfazz-product-hero-image-02.jpg'; ?>">
           </div>
 
-          <span class="product-section--hero-image__flag product-section--hero-image__flag--payfazz-blue">Easy</span>
+          <span class="product-section__hero-image__flag product-section__hero-image__flag--payfazz-blue">Easy</span>
         </div>
       </div>
     </div>
   </div>
-</div>
+</section>
 
-<div class="product-section product-section--masteragen">
+<section class="product-section product-section__solution">
   <div class="grid product-section__grid">
     <div class="grid__row">
-      <div class="grid__col product-section__col-5">
-        <img class="product-section__logo product-section__logo--masteragen" src="<?php echo get_template_directory_uri() . '/assets/img/product/logo-payfazz-masteragen-color.svg'; ?>" >
+      <div class="grid__col grid__col--12">
+        <h1>We are business<br>solution in your hand</h1>
+
+        <p class="product-section__paragraph">Be one of us and grow your business now<br> like all others. A look back at our milestones<br> to prove that we are solution you need.</p>
+      </div>
+
+      <div class="grid__col grid__col--12">
+        <div class="product-section__solution__box product-section__solution__box--one">
+          <p class="product-section__solution__box__title">15.000.000+<br><span>CONSUMERS</span></p>
+
+          <p class="product-section__solution__box__description">More than 15 milions customers<br>served by PAYFAZZ Merchants</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="grid__row">
+      <div class="grid__col product-section__solution__grid--offset-1">
+        <div class="product-section__solution__box product-section__solution__box--two">
+          <p class="product-section__solution__box__title">5.000.000+<br><span>DOWNLOADS</span></p>
+
+          <p class="product-section__solution__box__description">More than 15 milions customers<br>served by PAYFAZZ Merchants</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="grid__row">
+      <div class="grid__col flex-grow-0 flex-shrink-1 product-section__solution__grid--offset-2">
+        <div class="product-section__solution__box product-section__solution__box--three">
+          <p class="product-section__solution__box__title">400.000+&nbsp;&nbsp;<br><span>MERCHANTS</span></p>
+
+          <p class="product-section__solution__box__description">More than 15 milions customers<br>served by PAYFAZZ Merchants</p>
+        </div>
+      </div>
+
+      <div class="grid__col product-section__solution__grid--offset-3">
+        <div class="product-section__solution__box product-section__solution__box--four">
+          <p class="product-section__solution__box__title">100+<br><span>PARTNERS</span></p>
+
+          <p class="product-section__solution__box__description">More than 15 milions customers<br>served by PAYFAZZ Merchants</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="product-section product-section__masteragen">
+  <div class="grid product-section__grid">
+    <div class="grid__row">
+      <div class="grid__col grid__col--5">
+        <img class="product-section__masteragen__logo" src="<?php echo get_template_directory_uri() . '/assets/img/product/logo-payfazz-masteragen-color.svg'; ?>" >
 
         <h2 class="product-section__heading-two">One Stop App for Every Financial Service Needs</h2>
 
@@ -55,22 +103,22 @@ and start growing your business with Modal PAYFAZZ.</p>
         <a class="button button-outline--payfazz-blue">PELAJARI LEBIH</a>
       </div>
 
-      <div class="grid__col product-section__col-4 product-section__offset-2">
+      <div class="grid__col grid__col--4 grid__offset--2">
         <img src="<?php echo get_template_directory_uri() . '/assets/img/product/payfazz-masteragen-phone.png' ?>">
       </div>
     </div>
   </div>
-</div>
+</section>
 
-<div class="product-section product-section--agen">
+<section class="product-section product-section__agen">
   <div class="grid product-section__grid">
     <div class="grid__row">
-      <div class="grid__col product-section__col-5">
+      <div class="grid__col grid__col--5">
         <img src="<?php echo get_template_directory_uri() . '/assets/img/product/payfazz-agen-phone.png' ?>">
       </div>
 
-      <div class="grid__col product-section__col-5 product-section__offset-1">
-        <img class="product-section__logo product-section__logo--agen" src="<?php echo get_template_directory_uri() . '/assets/img/product/logo-payfazz-agen-color.svg'; ?>" >
+      <div class="grid__col grid__col--5 grid__offset--1">
+        <img class="product-section__agen__logo" src="<?php echo get_template_directory_uri() . '/assets/img/product/logo-payfazz-agen-color.svg'; ?>" >
 
         <h2 class="product-section__heading-two">One Stop App for Ordering<br>Digital &amp; Wholesale Goods</h2>
 
@@ -80,13 +128,13 @@ and start growing your business with Modal PAYFAZZ.</p>
       </div>
     </div>
   </div>
-</div>
+</section>
 
-<div class="product-section product-section--mobile">
+<section class="product-section product-section__mobile">
   <div class="grid product-section__grid">
     <div class="grid__row justify-content-between">
-      <div class="grid__col product-section__col-5">
-        <img class="product-section__logo product-section__logo--mobile" src="<?php echo get_template_directory_uri() . '/assets/img/product/logo-payfazz-mobile-color.svg'; ?>" >
+      <div class="grid__col grid__col--5">
+        <img class="product-section__mobile__logo" src="<?php echo get_template_directory_uri() . '/assets/img/product/logo-payfazz-mobile-color.svg'; ?>" >
 
         <h2 class="product-section__heading-two">One Stop App for Loyalty, Payments and Rewards</h2>
 
@@ -95,17 +143,17 @@ and start growing your business with Modal PAYFAZZ.</p>
         <a class="button button-outline--payfazz-blue">PELAJARI LEBIH</a>
       </div>
 
-      <div class="grid__col product-section__col-5">
+      <div class="grid__col grid__col--5">
         <img src="<?php echo get_template_directory_uri() . '/assets/img/product/payfazz-mobile-phone.png' ?>">
       </div>
     </div>
   </div>
-</div>
+</section>
 
-<section class="product-section product-section--testimonial">
+<section class="product-section product-section__testimonial">
   <div class="grid">
     <div class="grid__row justify-content-center">
-      <div class="grid__col product-section__col-8 text-center">
+      <div class="grid__col grid__col--8 text-center">
         <h1>Di unduh lebih dari<br>5.000.000 kali di seluruh Indonesia</h1>
       </div>
     </div>
