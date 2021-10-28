b6168ed53b2484b43def9bbba65d7ab75e74a998
DESKTOP-8JJ7F6C\Firdaus Riyanto <firdausriyanto@gmail.com>
2021-10-28 20:11:23 +0700

    add ajax load more

--- inc/enqueue-script.php
+++ inc/enqueue-script.php
@@ -3,9 +3,7 @@
 /* Including CSS & JavaScript.
 ** link@ https://developer.wordpress.org/themes/basics/including-css-javascript/
 **/
-function add_enqueue_scripts() {
-  global $wp_query; 
-
+function add_enqueue_scripts( $the_query ) {
   $theme_version = wp_get_theme()->get( 'Version' );
 
   // slick 
@@ -22,24 +20,39 @@ function add_enqueue_scripts() {
   wp_enqueue_style( 'main', $directory . 'assets/css/main.css', array(), $theme_version, 'all' );
 
   //
-  wp_enqueue_script( 'add-ajaxobject' );
-  wp_enqueue_script( 'scripts', $directory . 'assets/js/main.bundle.js', array(), $theme_version, true  );
-
-  if ( is_wp_error( $request ) ) {
-      return false; // Bail early
-  }
-
-  //
-  $body = wp_remote_retrieve_body( $request );
-  $json = utf8_encode( $body );
-  $data = json_decode( $json );
+  wp_register_script( 'scripts', $directory . 'assets/js/main.bundle.js', array( ), $theme_version );
 
   wp_localize_script( 'scripts', 'ajaxobject', array( 
-      'ajaxurl' => admin_url( 'admin-ajax.php' ),
-      'data' => $data,
-      'directory' => $directory,
-      'max' => $wp_query->max_num_pages
+      'ajaxurl'       => admin_url( 'admin-ajax.php' ),
+      'directory'     => $directory,
+      'posts'         => json_encode( $the_query->query_vars ),
+      'current_page'  => $the_query->query_vars['paged'] ? $the_query->query_vars['paged'] : 1,
+      'max_page'      => $the_query->max_num_pages,
     )
   );
+
+  wp_enqueue_script( 'scripts', '', '', '', true  );
 }
 add_action( 'wp_enqueue_scripts', 'add_enqueue_scripts' );
+
+
+function ajax_load_more() {
+    $args = json_decode( stripslashes( $_POST["query"] ), true );
+    $args['paged'] = $_POST["page"] + 1; // we need next page to be loaded
+    $args['posts_per_page'] = 4;
+    $args['post_status'] = 'publish';
+
+    query_posts( $args );
+
+    var_dump( $_POST );
+
+    if ( have_posts() ) :
+      while( have_posts() ): the_post();
+        get_template_part( 'template-parts/content', 'post' );
+      endwhile;
+    endif;
+
+    wp_die();
+}
+add_action( 'wp_ajax_ajax_load_more', 'ajax_load_more' );
+add_action(' wp_ajax_nopriv_ajax_load_more', 'ajax_load_more' );
